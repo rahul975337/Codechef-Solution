4b4986d926e384a5e6faa895fd58b64c984047c5
rahul975337 <tilwani03@gmail.com>
2020-10-18 19:23:58 +0530

    done

--- perfectNumbers.java
+++ perfectNumbers.java
@@ -26,11 +26,8 @@ public class perfectNumbers {
     }
 
     ///////////////////////////////////////////
-    // Returns true if n is perfect
     static boolean isPerfect(int n) {
-        // To store sum of divisors
         int sum = 1;
-        // Find all divisors and add them
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 if (i * i != n)
@@ -39,8 +36,7 @@ public class perfectNumbers {
                     sum = sum + i;
             }
         }
-        // If sum of divisors is equal to
-        // n, then n is a perfect number
+
         if (sum == n && n != 1)
             return true;
         return false;
